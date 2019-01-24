ed2caad285d419c8acb9846d258586ed2b5181cc
Thomas Ingram <thomasingram@vertx.xyz>
2019-01-24 16:10:21 +1100

    Started collecting Panels to draw in

--- com.vertx.nKittens/KittensEditorTool.cs
+++ com.vertx.nKittens/KittensEditorTool.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor;
 using UnityEditor.EditorTools;
 using UnityEngine;
@@ -13,10 +16,7 @@ namespace Vertx
 		private GUIContent _iconContent;
 		public override GUIContent toolbarIcon => _iconContent;
 
-		void OnEnable()
-		{
-			_iconContent = new GUIContent(LoadTextureFromString(KittensImages.Icon), "🐱");
-		}
+		void OnEnable() => _iconContent = new GUIContent(LoadTextureFromString(KittensImages.Icon), "Kittens!");
 
 		private static Texture2D LoadTextureFromString(string s)
 		{
@@ -26,14 +26,57 @@ namespace Vertx
 			return tex;
 		}
 
+		public override void OnActivate()
+		{
+			DiscoverAllRootContexts();
+		}
+
 		public override void OnToolGUI(EditorWindow window)
 		{
 			
 		}
 
-		List<VisualElement> DiscoverAllWindowContexts()
+		List<VisualElement> DiscoverAllRootContexts()
 		{
+			Type uiElementsUtilityType = Type.GetType("UnityEngine.UIElements.UIElementsUtility,UnityEngine");
+			if (!DebugRelevantError(uiElementsUtilityType, "UnityEngine.UIElements.UIElementsUtility", "type"))
+				return null;
+
+			Type panelType = Type.GetType("UnityEngine.UIElements.Panel,UnityEngine");
+			if (!DebugRelevantError(panelType, "UnityEngine.UIElements.Panel", "type"))
+				return null;
+			
+			Type dictionaryType = typeof(KeyValuePair<,>);
+			Type genericDictionaryType = dictionaryType.MakeGenericType(typeof(int), panelType);
+			PropertyInfo kvpValueProperty = genericDictionaryType.GetProperty("Value", BindingFlags.Public | BindingFlags.Instance);
+			if (!DebugRelevantError(kvpValueProperty, "KeyValuePair<int,Panel>.Value", "Property"))
+				return null;
+
+			MethodInfo GetPanelsIteratorMethod = uiElementsUtilityType.GetMethod("GetPanelsIterator", BindingFlags.Static | BindingFlags.NonPublic);
+			if (!DebugRelevantError(GetPanelsIteratorMethod, "UIElementsUtility.GetPanelsIterator", "Method"))
+				return null;
+
+			IEnumerator enumerator = (IEnumerator) GetPanelsIteratorMethod.Invoke(null, null);
+
+			//Get all panels to inject content into
+			while (enumerator.MoveNext())
+			{
+				object enumeratorCurrent = enumerator.Current;
+				object value = kvpValueProperty.GetValue(enumeratorCurrent);
+				Debug.Log(value);
+			}
+			
 			return null;
+
+			bool DebugRelevantError(object o, string name, string type)
+			{
+				if (o == null)
+				{
+					Debug.LogError($"{type} {name} not found.");
+					return false;
+				}
+				return true;
+			}
 		}
 
 		private void OnDisable()
