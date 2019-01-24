95bd854b321c09b0ced7a0c2af3e07f04831455a
Thomas Ingram <thomasingram@vertx.xyz>
2019-01-24 15:02:19 +1100

    Added Editor Tool /w icon.

---
+++ com.vertx.nKittens/KittensEditorTool.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Vertx.Constants;
+
+namespace Vertx
+{
+	[EditorTool("Kittens!")]
+	public class KittensEditorTool : EditorTool
+	{
+		private GUIContent _iconContent;
+		public override GUIContent toolbarIcon => _iconContent;
+
+		void OnEnable()
+		{
+			_iconContent = new GUIContent(LoadTextureFromString(KittensImages.Icon), "🐱");
+		}
+
+		private static Texture2D LoadTextureFromString(string s)
+		{
+			byte[] rawTextureData = System.Convert.FromBase64String(s);
+			Texture2D tex = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+			tex.LoadImage(rawTextureData);
+			return tex;
+		}
+
+		public override void OnToolGUI(EditorWindow window)
+		{
+			
+		}
+
+		List<VisualElement> DiscoverAllWindowContexts()
+		{
+			return null;
+		}
+
+		private void OnDisable()
+		{
+			DestroyImmediate(_iconContent.image);
+		}
+	}
+}
